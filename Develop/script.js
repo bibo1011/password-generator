91fc105f300ff32e4390f2d578b433638ee940d5
Blue Abad <bibo1011@gmail.com>
2020-08-06 19:21:33 -0700

    add working pwlength prompt

--- Develop/script.js
+++ Develop/script.js
@@ -6,42 +6,36 @@
 // Generate a password that matches all the criteria selected
 // Password generated is displayed: alert or on page
 
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // On click, series of prompts for password criteria
 // Prompt for password criteria to include
 // Prompt for length of password: 8 to 128 characters
-var pwLength = function(number) {
-  while (pwLength < 8 || pwLength > 128) {
-    prompt("Give password length (password has to be between 8 - 128 characters only)");
-    console.log(pwLength);
-    if (pwLength >= 8 || pwLength <= 128) {
-    charTypes();
-    } 
-  }
-}
-  
 // Prompt for character types: lowercase, uppercase, numeric and/or special characters
-var charTypes = function(){
+
+var criteria = function(){
+  var pwLength = prompt("Give password length (password has to be between 8 - 128 characters only)", "8"); 
+  console.log(criteria);
+
   var pwUpperCase = prompt("Do you want to use uppercase characters?");
   console.log(pwUpperCase);
+
   var pwLowerCase = prompt("Do you want to use lowercase characters?");
   console.log(pwLowerCase);
+
   var pwNumeric = prompt("Do you want to use numeric characters?");
   console.log(pwNumeric);
+
   var pwSpecial = prompt("Do you want it to use special characters?");
   console.log(pwSpecial);
-}
+  
+}   
 
-// for (var i = 0; i < pwLength; i++){
- 
 
-    // } else {
-    // charTypes();
-// }
-// pwLength()
-// charTypes()
+
+
 
 
 
@@ -49,12 +43,6 @@ var charTypes = function(){
 // var generateBtn = document.getElementById("generate");
 generateBtn.addEventListener("click", criteria);
 // generateBtn.addEventListener("click", someOtherFunction);
-
-// function criteria() {
-  // prompt ("select criteria");
-  // var lengthPassword = prompt ("choose length of password between 8 to 128");
-// }
-  
 
 
 
